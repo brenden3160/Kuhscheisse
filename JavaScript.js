0f833a72567d2138f92fa0cf6942f22ba8abd28b
brenden3160 <43614374+brenden3160@users.noreply.github.com>
2018-11-06 13:42:10 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,7 @@
-function myButton () {
+function myButton() {
 //references my button//
   var doy= new Date();
   var n = d.toLocalTimeString(); 
+  document.getElementById("demo").innerHTML = n;
   myButton();
 }
