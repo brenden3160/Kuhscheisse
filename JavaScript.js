e6127290f6ad4ba84663029963cc4d02992b0eca
brenden3160 <43614374+brenden3160@users.noreply.github.com>
2018-11-20 13:29:07 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -15,4 +15,6 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-
+function visitPage(){
+        window.location='http://www.example.com';
+    }
